21c83d2509428c2a8736027d486bb4060e39cd17
Ben Mason <ben@wattmason.co.uk>
2022-02-02 12:25:22 +0000

    update domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(subsquid-khala.litentry.io)
+domains=(squid.litentry.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
