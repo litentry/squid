e1db7c48991530755ad5ad1ad8a8063a59784451
Ben Mason <ben@wattmason.co.uk>
2022-02-10 14:21:52 +0000

    Add balance.Transfer event to polkadoot & kusama types

--- src/types/kusama/events.ts
+++ src/types/kusama/events.ts
@@ -1,5 +1,67 @@
 import assert from 'assert'
 import {EventContext, Result, deprecateLatest} from './support'
+import * as v9130 from './v9130'
+
+export class BalancesTransferEvent {
+  constructor(private ctx: EventContext) {
+    assert(this.ctx.event.name === 'balances.Transfer')
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value, fees).
+   */
+  get isV1020(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === '154fca303841d334782de2e871e3572f786f81f86e2a6153c2b9e8dc6fc27422'
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value, fees).
+   */
+  get asV1020(): [Uint8Array, Uint8Array, bigint, bigint] {
+    assert(this.isV1020)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value).
+   */
+  get isV1050(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === '9611bd6b933331f197e8fa73bac36184681838292120987fec97092ae037d1c8'
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value).
+   */
+  get asV1050(): [Uint8Array, Uint8Array, bigint] {
+    assert(this.isV1050)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  /**
+   * Transfer succeeded.
+   */
+  get isV9130(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === '99bc4786247456e0d4a44373efe405e598bfadfac87a7c41b0a82a91296836c1'
+  }
+
+  /**
+   * Transfer succeeded.
+   */
+  get asV9130(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
+    assert(this.isV9130)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  get isLatest(): boolean {
+    deprecateLatest()
+    return this.isV9130
+  }
+
+  get asLatest(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
+    deprecateLatest()
+    return this.asV9130
+  }
+}
 
 export class CrowdloanContributedEvent {
   constructor(private ctx: EventContext) {
