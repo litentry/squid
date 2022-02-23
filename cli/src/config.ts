f6363224e83fb363151f691b41db3cbf0cfbbfd2
Mark Davison <github@iammark.co.uk>
2022-02-23 00:53:35 +0000

    Move make commands to devkit

---
+++ cli/src/config.ts
@@ -0,0 +1,16 @@
+export default {
+  "chains": {
+    "polkadot": {
+      "chain": "wss://polkadot.api.onfinality.io/public-ws",
+      "archive": "https://polkadot-squid-archive.litentry.io/graphql/v1/graphql"
+    },
+    "khala": {
+      "chain": "wss://khala.api.onfinality.io/public-ws",
+      "archive": "https://khala-squid-archive.litentry.io/graphql/v1/graphql"
+    },
+    "kusama": {
+      "chain": "wss://kusama.api.onfinality.io/public-ws",
+      "archive": "https://kusama.indexer.gc.subsquid.io/v4/graphql"
+    }
+  }
+}
