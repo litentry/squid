e59ae28d3fd23b785cd0e1f8a10c8a3d43a808b9
Mark Davison <github@iammark.co.uk>
2022-03-13 13:17:35 +0000

    Add snapshot make command

---
+++ cli/src/commands/snapshot/make.ts
@@ -0,0 +1,118 @@
+import { Command, Flags } from '@oclif/core';
+import { existsSync, mkdirSync, unlinkSync, writeFileSync } from 'fs';
+import { execSync } from 'child_process';
+import getProjectIndexingProgress from '../../getProjectIndexingProgress';
+import cli from 'cli-ux';
+import { promises as fs } from 'fs';
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+export default class MakeSnapshot extends Command {
+
+  private module: string = '';
+  private version: string = '';
+
+  static description = 'Make a snapshot of a prawn';
+
+  static examples = [
+    `$ devkit snapshot make balances`
+  ];
+
+  static args = [{ name: 'module', description: 'Module to deploy', required: true }];
+
+  async run(): Promise<void> {
+    const { args, flags } = await this.parse(MakeSnapshot);
+    this.module = args.module;
+
+    if (!this.moduleExists()) {
+      return this.error(`Module ${this.module} does not exist`);
+    }
+
+    this.log('Pull latest changes:');
+    this.gitPull();
+
+    this.version = this.getVersion();
+
+    this.log('Start indexing:');
+    this.startIndexing();
+
+    let indexingComplete = false;
+    while (!indexingComplete) {
+      await this.sleep(10000);
+      const containers = await getProjectIndexingProgress(this.getProjectName());
+      indexingComplete = containers.every((container) => container.progress === 1);
+      cli.table(containers, { name: {}, progress: {} });
+    }
+
+    this.log('Indexing complete');
+
+    this.log('Stop indexers and database');
+    this.stopIndexing();
+
+    await this.sleep(2000);
+
+    this.log('Create tarball');
+    this.createTarball();
+
+    this.log('Upload snapshot');
+    this.uploadSnapshot();
+
+    this.log('Delete db data');
+    this.deleteDbData();
+  }
+
+  private getVersion = () => execSync('git rev-parse --short HEAD').toString().trim();
+
+  private gitPull = () => execSync('git pull', { stdio: 'inherit' });
+
+  private getProjectRootDir = () => `${__dirname}/../../../..`;
+
+  private getModuleDir = () => `${this.getProjectRootDir()}/prawns/${this.module}`;
+
+  private getDbDataDir = () => `${this.getProjectRootDir()}/data/db/`;
+
+  private moduleExists = () => existsSync(this.getModuleDir());
+
+  private sleep = (time: number) => new Promise(r => setTimeout(r, time));
+
+  private getProjectName = () => `${this.module}_${this.version}`;
+
+  private getSnapshotName = () => `${this.getProjectName()}.tar.gz`;
+
+  private startIndexing = () => execSync(
+    `COMPOSE_PROJECT_NAME=${this.getProjectName()} docker-compose --profile indexing up -d`,
+    { cwd: this.getModuleDir(), stdio: 'inherit' }
+  );
+
+  private stopIndexing = () => execSync(
+    `COMPOSE_PROJECT_NAME=${this.getProjectName()} docker-compose down`,
+    { cwd: this.getModuleDir(), stdio: 'inherit' }
+  );
+
+  private createTarball = () => execSync(
+    `tar -zcvf ${this.getSnapshotName()} ${this.getProjectName()}`,
+    { cwd: this.getDbDataDir(), stdio: 'inherit' }
+  );
+
+  private uploadSnapshot = async () => {
+    const fileName = `${this.getDbDataDir()}/${this.getSnapshotName()}`;
+    const data = await fs.readFile(fileName);
+    const params = {
+      Bucket: 'litentry-db-backup',
+      Key: 'contacts.csv',
+      Body: JSON.stringify(data, null, 2)
+    };
+    await s3.upload(params).promise();
+  };
+
+  private deleteDbData = () => {
+    unlinkSync(`${this.getDbDataDir()}/${this.getSnapshotName()}`); //Delete tar.gz file
+    unlinkSync(`${this.getDbDataDir()}/${this.getProjectName()}`); //Delete db folder
+  };
+
+
+}
