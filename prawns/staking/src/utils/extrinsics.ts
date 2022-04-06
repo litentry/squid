d9c84a690bd01d7097e1e3270c0f69509dadb459
João D <joao@litentry.com>
2022-04-06 11:01:00 +0100

    cleanup

--- prawns/staking/src/utils/extrinsics.ts
+++
@@ -1,5 +0,0 @@
-import { ExtrinsicArg } from '@subsquid/substrate-processor';
-
-export function getFieldByNameFromExtrinsicArgs(args: ExtrinsicArg[] | undefined, name: string): any {
-  return (args && args.find(arg => arg.name === name))?.value;
-}
