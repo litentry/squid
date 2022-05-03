140b27ef2f1f863f2a7ee4c0860d839bf65c3214
João D <joao@litentry.com>
2022-05-03 08:44:59 +0100

    Add checks and rounding to the deploy mechanism

--- cli/src/getProjectIndexingProgress.ts
+++ cli/src/getProjectIndexingProgress.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { execSync } from 'child_process';
 
+const ROUNDING_MULTIPLIER = 100000000;
+const ROUNDING_DIVIDER = ROUNDING_MULTIPLIER / 100;
+
 const getIndexingProcessorContainers = (projectName: string) => {
   const containerRows = execSync(`docker ps --filter label='com.docker.compose.project=${projectName}' --filter expose=3000 --format='{{.ID}},{{.Names}}'`).toString().trim().split('\n');
 
@@ -14,12 +17,12 @@ const getIndexingProcessorContainers = (projectName: string) => {
 export default async (projectName: string) => Promise.all(
   getIndexingProcessorContainers(projectName).map(async (container) => {
     const chainHeight = (await axios.get(`http://${container.host}/metrics/sqd_processor_chain_height`)).data.toString();
-    const chainHeightMatches = /([\d.]+)/g.exec(chainHeight) || [0];
+    const chainHeightMatches = /([\d.]+)/g.exec(chainHeight)?.[0] || 0;
 
     const lastBlock = (await axios.get(`http://${container.host}/metrics/sqd_processor_last_block`)).data.toString();
-    const lastBlockMatches = /([\d.]+)/g.exec(lastBlock) || [0];
+    const lastBlockMatches = /([\d.]+)/g.exec(lastBlock)?.[0] || 1;
 
-    const progress = Number(lastBlockMatches[0]) / Number(chainHeightMatches[0]);
+    const progress = Math.round(Number(lastBlockMatches) / Number(chainHeightMatches) * ROUNDING_MULTIPLIER) / ROUNDING_DIVIDER;
 
     return { ...container, progress };
 }));
