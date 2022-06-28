8f10237bcb0d369bbcf5da5207d9114996a3ec19
Mark Davison <github@iammark.co.uk>
2022-06-28 21:01:16 +0100

    Handle democracy proposal cancel (#99)
    * Handle cancel status for democracy proposals
    
    * Remove debugging
    
    * Add defensive code around the risk of multiple ProposedEvents in the same batchAll extrinsic

--- prawns/governance/src/handlers/democracy.Proposed.event.ts
+++ prawns/governance/src/handlers/democracy.Proposed.event.ts
@@ -1,8 +1,37 @@
-import { EventHandlerContext } from '@subsquid/substrate-processor';
-import { decodeAddress } from '../utils';
-import { SubstrateNetwork, SubstrateDemocracyProposal } from '../model';
-import { getOrCreateGovernanceAccount } from '../utils';
+import { EventHandlerContext, ExtrinsicArg } from '@subsquid/substrate-processor';
+import { decodeAddress, getOrCreateGovernanceAccount } from '../utils';
+import { SubstrateDemocracyProposal, SubstrateNetwork } from '../model';
 import { getDemocracyProposedEvent } from './typeGetters/getDemocracyProposedEvent';
+
+const getProposalHash = (ctx: EventHandlerContext, network: SubstrateNetwork) => {
+
+  const args = ctx.event!.extrinsic!.args;
+
+  if (ctx.event.extrinsic?.method === 'batchAll') {
+    const calls = args[0].value as { args: {[key: string]: any} }[];
+    const batchProposeArgs = calls.filter(call => call.args && call.args.proposal_hash);
+    if (batchProposeArgs.length === 0) {
+      throw new Error(`Unable to find propose args in batch in block ${ctx.block.height}`);
+    }
+    if (batchProposeArgs.length > 1) {
+      throw new Error(`Found multiple proposal_hash args in batch in block ${ctx.block.height}`);
+    }
+    return batchProposeArgs[0].args.proposal_hash;
+  } else {
+
+    const proposalHashArg = args.find(arg => arg.name === 'proposal_hash' || arg.name === 'proposalHash');
+
+    if (proposalHashArg) {
+      return proposalHashArg.value as string;
+    }
+  }
+
+  if (network === SubstrateNetwork.kusama && (ctx.block.height === 25947 || ctx.block.height === 37922)) {
+    return '';
+  }
+
+  throw new Error(`Failed to find proposalHash`);
+};
 
 export default (network: SubstrateNetwork) =>
   async (ctx: EventHandlerContext) => {
@@ -15,16 +44,12 @@ export default (network: SubstrateNetwork) =>
     const date = new Date(ctx.block.timestamp);
     const rootAccount = decodeAddress(ctx.event.extrinsic.signer);
     const event = getDemocracyProposedEvent(ctx, network);
-    const proposalHashArg = ctx.event.extrinsic.args.find(arg => arg.name === 'proposal_hash' || arg.name === 'proposalHash');
-
-    if (!proposalHashArg) {
-      throw new Error(`Failed to find proposalHash`);
-    }
+    const proposalHash = getProposalHash(ctx, network);
 
     const account = await getOrCreateGovernanceAccount(ctx.store, {
       id: ctx.event.extrinsic.signer,
       rootAccount,
-      network,
+      network
     });
     account.totalDemocracyProposals = account.totalDemocracyProposals + 1;
     account.totalProposals = account.totalDemocracyProposals; // Deprecated
@@ -38,7 +63,7 @@ export default (network: SubstrateNetwork) =>
       blockNumber,
       date,
       updatedAt: date,
-      proposalHash: proposalHashArg.value as string,
+      proposalHash,
       proposalIndex: event.proposalIndex,
       depositAmount: event.deposit,
       status: 'proposed'
