5996e52fda3ad8ba41beb20732a737701a1fa94a
Mark Davison <github@iammark.co.uk>
2022-06-28 02:02:19 +0100

    Remove debugging

--- prawns/governance/src/handlers/democracy.ClearPublicProposals.extrinsic.ts
+++ prawns/governance/src/handlers/democracy.ClearPublicProposals.extrinsic.ts
@@ -4,10 +4,7 @@ import substrateDemocracyProposalRepository from '../repositories/substrateDemoc
 
 export default (network: SubstrateNetwork) =>
   async (ctx: ExtrinsicHandlerContext) => {
-  console.log('3234');
     const proposedProposals = await substrateDemocracyProposalRepository.findByStatus(ctx, network, 'proposed');
-    console.log(proposedProposals);
-    console.log(ctx.block.height, 'fooooooo');
     await Promise.all(proposedProposals.map(proposal => {
       proposal.status = 'cancelled';
       return ctx.store.save(proposal);
