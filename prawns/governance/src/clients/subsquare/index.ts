a1e3d6b30ee2e65de0d128d02318aa2a928247a2
Mark Davison <github@iammark.co.uk>
2022-07-04 09:55:04 +0100

    Fix for phala/khala bug in the integration with subsquare

--- prawns/governance/src/clients/subsquare/index.ts
+++ prawns/governance/src/clients/subsquare/index.ts
@@ -2,8 +2,13 @@ import fetch from 'node-fetch';
 import { SubstrateNetwork } from '../../model';
 import { SubsquareDemocracyProposal } from './types';
 
+const getBaseUrl = (network: SubstrateNetwork) => {
+  const subdomain = network === SubstrateNetwork.phala ? 'khala' : network;
+  return `https://${subdomain}.subsquare.io/api/`;
+}
+
 const getDemocracyProposal = async (network: SubstrateNetwork, proposalIndex: number): Promise<SubsquareDemocracyProposal> => {
-  const response = await fetch(`https://${network}.subsquare.io/api/democracy/proposals/${proposalIndex}`);
+  const response = await fetch(`${getBaseUrl(network)}/democracy/proposals/${proposalIndex}`);
   return await response.json() as unknown as SubsquareDemocracyProposal;
 }
 
