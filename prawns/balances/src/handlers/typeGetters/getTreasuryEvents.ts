fc3e871c200450d2641d275f67c95a4756b79341
Ben Mason <ben@wattmason.co.uk>
2022-08-02 09:08:47 +0100

    Cleanup treasury deposit removal

--- prawns/balances/src/handlers/typeGetters/getTreasuryEvents.ts
+++ prawns/balances/src/handlers/typeGetters/getTreasuryEvents.ts
@@ -1,69 +1,8 @@
 import { EventHandlerContext } from '@subsquid/substrate-processor';
 import { SubstrateNetwork } from '../../model';
-import {
-  TreasuryAwardedEvent as KhalaTreasuryAwardedEvent,
-  TreasuryDepositEvent as KhalaTreasuryDepositEvent,
-} from '../../types/khala/events';
-import {
-  TreasuryAwardedEvent as LitentryTreasuryAwardedEvent,
-  TreasuryDepositEvent as LitentryTreasuryDepositEvent,
-} from '../../types/litentry/events';
-
-import {
-  TreasuryAwardedEvent as LitmusTreasuryAwardedEvent,
-  TreasuryDepositEvent as LitmusTreasuryDepositEvent,
-} from '../../types/litmus/events';
-
-export function getTreasuryDepositEvent(
-  ctx: EventHandlerContext,
-  network: SubstrateNetwork
-): bigint {
-  switch (network) {
-    case SubstrateNetwork.phala: {
-      const event = new KhalaTreasuryDepositEvent(ctx);
-
-      if (event.isV1) {
-        return event.asV1;
-      } else if (event.isV1110) {
-        return event.asV1110.value;
-      } else {
-        return event.asLatest.value;
-      }
-    }
-
-    case SubstrateNetwork.litmus: {
-      const event = new LitmusTreasuryDepositEvent(ctx);
-
-      if (event.isV9020) {
-        return event.asV9020;
-      }
-
-      if (event.isV9031) {
-        return event.asV9031.value;
-      }
-
-      return event.asLatest.value;
-    }
-
-    case SubstrateNetwork.litentry: {
-      const event = new LitentryTreasuryDepositEvent(ctx);
-
-      if (event.isV9000) {
-        return event.asV9000;
-      }
-
-      if (event.isV9071) {
-        return event.asV9071.value;
-      }
-
-      return event.asLatest.value;
-    }
-
-    default: {
-      throw new Error('getTreasuryDepositEvent::network not supported');
-    }
-  }
-}
+import { TreasuryAwardedEvent as KhalaTreasuryAwardedEvent } from '../../types/khala/events';
+import { TreasuryAwardedEvent as LitentryTreasuryAwardedEvent } from '../../types/litentry/events';
+import { TreasuryAwardedEvent as LitmusTreasuryAwardedEvent } from '../../types/litmus/events';
 
 export function getTreasuryAwardedEvent(
   ctx: EventHandlerContext,
